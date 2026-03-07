c4cbee0c0236aed954dcdea3cb2147750e727f8b
pabordonez <46683090+pabordonez@users.noreply.github.com>
2026-03-07 12:15:09 +0100

    Feature fix camel case file (#72)
    Fix camelCase

--- apps/api/tests/useCases/ProductTransactionUseCases.test.ts
+++ apps/api/tests/useCases/ProductTransactionUseCases.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { ProductTransactionUseCases } from '../../src/application/useCases/productTransactionUseCases'
+import { ProductTransactionUseCases } from '../../src/application/useCases/ProductTransactionUseCases_tmp'
 import { IProductTransactionRepository } from '../../src/domain/repository/IProductTransactionRepository'
 import { IProductRepository } from '../../src/domain/repository/IProductRepository'
 
